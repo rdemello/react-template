28de96e97009a1388a328063382527f4de42c03c
Ryan de Mello <demez884@hotmail.com>
2023-12-12 19:02:39 +0000

    cleaned up file

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: `http://localhost:${PORT}`,
+                changeOrigin: true,
+            },
+        },
     },
-  },
-  build: {
-    outDir: "dist/app",
-  },
+    build: {
+        outDir: 'dist/app',
+    },
 });
