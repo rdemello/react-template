46996846ac8fe5c34f98567e6428ca291b35c867
Ryan de Mello <demez884@hotmail.com>
2024-01-05 12:54:10 +0000

    Fixed routing and server files

--- src/server.cjs
+++ src/server.cjs
@@ -13,14 +13,21 @@ app.use(express.json());
 app.use('/api', router);
 
 // Serve app production bundle
-// app.use(express.static('/src/app'));
-
-// Handle client routing, return all requests to the app
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));   // - build
-    // res.sendFile(path.join(__dirname, '../index.html'));      // - dev
+app.use(express.static(path.join(__dirname, '../dist'))).listen(PORT, () =>
+    console.log(`Server listening at http://localhost:${PORT}`),
+);
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
+
+// Handles general requests by redirecting to start of app
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
 });
+
+
